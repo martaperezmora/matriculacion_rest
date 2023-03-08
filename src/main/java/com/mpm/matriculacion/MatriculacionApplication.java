50685c6c802f83aa0cabd986f9bab5d43c3ffe67
Marta Perez <mpermor2205@g.educaand.es>
2023-03-08 08:40:31 +0100

    commit inicial

---
+++ src/main/java/com/mpm/matriculacion/MatriculacionApplication.java
@@ -0,0 +1,13 @@
+package com.mpm.matriculacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatriculacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatriculacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mpm/matriculacion/MatriculacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpm.matriculacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatriculacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
